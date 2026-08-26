cbc6a5c1487fa98e87dab9a0353292067e5fa1cc
PrimalLuxe <Olympustuber@gmail.com>
2026-08-26 12:42:11 -0500

    Add centralized sound manifest

---
+++ shake-vending-game/src/ReplicatedStorage/Shared/SoundManifest.lua
@@ -0,0 +1,18 @@
+local SoundManifest = {
+    Machine = {
+        Hum = {Id="rbxassetid://171186876", Volume=0.16, Group="Ambient", Looped=true, RollOffMaxDistance=34},
+        Rattle = {Id="rbxassetid://9116540638", Volume=0.30, Group="SFX", PlaybackSpeed=1.04},
+        Clunk = {Id="rbxassetid://9116710986", Volume=0.52, Group="SFX"},
+    },
+    Rarity = {
+        Rare={Id="rbxassetid://9113155656",Volume=0.26,Group="RareReveal",PlaybackSpeed=1.00},
+        Epic={Id="rbxassetid://9113155656",Volume=0.31,Group="RareReveal",PlaybackSpeed=0.98},
+        Legendary={Id="rbxassetid://9113155656",Volume=0.38,Group="RareReveal",PlaybackSpeed=0.95},
+        Mythic={Id="rbxassetid://9113155656",Volume=0.43,Group="RareReveal",PlaybackSpeed=0.92},
+        Divine={Id="rbxassetid://9113155656",Volume=0.49,Group="RareReveal",PlaybackSpeed=0.89},
+        Secret={Id="rbxassetid://9113155656",Volume=0.55,Group="RareReveal",PlaybackSpeed=0.85},
+        Global={Id="rbxassetid://9113155656",Volume=0.62,Group="RareReveal",PlaybackSpeed=0.81},
+    },
+}
+
+return SoundManifest
