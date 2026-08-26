89ae8bdcf265b3fa2c67b9f736a949670baf8b47
PrimalLuxe <Olympustuber@gmail.com>
2026-08-26 12:44:41 -0500

    Persist remote contract in runnable source tree

---
+++ shake-vending-game/src/ReplicatedStorage/Shared/RemoteNames.lua
@@ -0,0 +1,6 @@
+return {
+    Events = {
+        ShakeMachine="ShakeMachine",CollectDrop="CollectDrop",DropSpawned="DropSpawned",WorldDrop="WorldDrop",Toast="Toast",OpenPanel="OpenPanel",EquipCosmetic="EquipCosmetic",SetShowcase="SetShowcase",InventoryAction="InventoryAction",OutfitAction="OutfitAction",UpgradeAction="UpgradeAction",SettingsAction="SettingsAction",ProgressionAction="ProgressionAction",RareGlobal="RareGlobal",EventChanged="EventChanged",RequestTrade="RequestTrade",TradeAction="TradeAction",TradeState="TradeState",
+    },
+    Functions = {GetSnapshot="GetSnapshot",GetHourlyBoard="GetHourlyBoard",GetPlayerSummary="GetPlayerSummary"},
+}
